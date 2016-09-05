bd3a19a240d23951e970d0e474f56afdb31c173e
joshuarichardson <richardsonjoshua228@gmail.com>
2016-09-05 12:11:07 +0100

    no message

--- src/com/eagles13/rut_downloader/Main.kt
+++ src/com/eagles13/rut_downloader/Main.kt
@@ -34,7 +34,7 @@ fun getFiles(dir: String) {
 fun main(args: Array<String>) {
     val downloadDirectory = "D:\\Download"
     val gson = Gson()
-    var details : ArrayList<String>? = gson.fromJson(Scanner(File("src/com/eagles13/rut_downloader/credentials.txt")).useDelimiter("\\Z").next(), object : TypeToken<ArrayList<String>>(){}.type)
+    val details : ArrayList<String>? = gson.fromJson(Scanner(File("src/com/eagles13/rut_downloader/credentials.txt")).useDelimiter("\\Z").next(), object : TypeToken<ArrayList<String>>(){}.type)
 
     val t = Thread(Runnable {
         if (details != null) {
@@ -86,44 +86,7 @@ fun main(args: Array<String>) {
 
 
 
-    /*    FTPClient ftp = new FTPClient();
-    FTPClientConfig config = new FTPClientConfig();
-    config.setXXX(YYY); // change required options
-    // for example config.setServerTimeZoneId("Pacific/Pitcairn")
-    ftp.configure(config );
-    boolean error = false;
-    try {
-      int reply;
-      String server = "ftp.example.com";
-      ftp.connect(server);
-      System.out.println("Connected to " + server + ".");
-      System.out.print(ftp.getReplyString());
 
-      // After connection attempt, you should check the reply code to verify
-      // success.
-      reply = ftp.getReplyCode();
-
-      if(!FTPReply.isPositiveCompletion(reply)) {
-        ftp.disconnect();
-        System.err.println("FTP server refused connection.");
-        System.exit(1);
-      }
-      ... // transfer files
-      ftp.logout();
-    } catch(IOException e) {
-      error = true;
-      e.printStackTrace();
-    } finally {
-      if(ftp.isConnected()) {
-        try {
-          ftp.disconnect();
-        } catch(IOException ioe) {
-          // do nothing
-        }
-      }
-      System.exit(error ? 1 : 0);
-    }
-    */
 }
 
 
