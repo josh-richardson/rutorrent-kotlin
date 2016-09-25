928378565928f90a7949c378845e7467173513ce
joshuarichardson <richardsonjoshua228@gmail.com>
2016-09-25 17:26:29 +0100

    Adds LFTP support

--- src/com/eagles13/rut_downloader/Main.kt
+++ src/com/eagles13/rut_downloader/Main.kt
@@ -2,13 +2,15 @@ package com.eagles13.rut_downloader
 
 import com.eagles13.rutorrent.Torrent
 import com.eagles13.rutorrent.ruTorrent
-import com.google.gson.Gson
+import com.google.gson.*
 import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
 import org.apache.commons.net.ftp.FTP
 import org.apache.commons.net.ftp.FTPClient
-import java.io.BufferedOutputStream
-import java.io.File
-import java.io.FileOutputStream
+import java.io.*
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.*
 import java.util.concurrent.Executors
 
@@ -16,32 +18,36 @@ import java.util.concurrent.Executors
 /**
  * Created by Joshua on 12/06/2016.
  */
-inline fun <reified T> genericType() = object: TypeToken<T>() {}.type!!
+inline fun <reified T> genericType() = object : TypeToken<T>() {}.type!!
+
+var cliDownload = false
+val downloadDirectory = "/home/joshua/Downloads"
+val gson = Gson()
+val downloaded = ArrayList<Torrent>()
+val downloading = ArrayList<Torrent>()
+
 
 fun main(args: Array<String>) {
-    val downloadDirectory = "D:\\Download"
-    val gson = Gson()
-    val details : ArrayList<String>? = gson.fromJson(Scanner(File("src/com/eagles13/rut_downloader/credentials.txt")).useDelimiter("\\Z").next(), object : TypeToken<ArrayList<String>>(){}.type)
+    try {
+        JsonParser().parse(String(Files.readAllBytes(Paths.get("rut-downloader.json")))).asJsonArray.forEach { downloaded.add(gson.fromJson(it, genericType<Torrent>())) }
+    } catch (e: Exception) {}
 
     val t = Thread(Runnable {
-        if (details != null) {
-            val downloaded = ArrayList<Torrent>()
-            val downloading = ArrayList<Torrent>()
-
-            val instance= ruTorrent(details[0], details[1], details[2])
-
-            while (true) {
-                instance.getTorrents().filter { x -> x.isDone }.forEach {
-//                  tests serialization
-                    val rit : Torrent = gson.fromJson(gson.toJson(it), genericType<Torrent>())
-                    println(rit)
-                    if (!downloaded.contains(it) && !downloading.contains(it)) {
-                        downloading.add(it)
+        val instance = ruTorrent(args[0], args[1], args[2])
+        val ftpDetails = FTPCredentials(args[3], args[4], args[5], args[6])
+        while (true) {
+            instance.getTorrents().filter { x -> x.isDone }.forEach {
+                if (!downloaded.contains(it) && !downloading.contains(it)) {
+                    downloading.add(it)
+                    if (cliDownload) {
+                        cliDownloadTorrent(it, ftpDetails)
+                    } else {
                         val ftp = FTPClient()
-                        ftp.connect(details[3])
-                        ftp.login(details[4], details[5])
+                        ftp.connect(args[3])
+                        ftp.login(args[4], args[5])
                         ftp.enterLocalPassiveMode()
                         ftp.setFileType(FTP.BINARY_FILE_TYPE)
+                        ftp.setReceiveBufferSize(1048576)
                         println(ftp.replyCode)
                         getFilesRecursive(it.location, ftp).forEach { fi, isDirectory ->
                             if (!isDirectory) {
@@ -57,9 +63,13 @@ fun main(args: Array<String>) {
                             }
                         }
                     }
+                    downloaded.add(it)
+                    downloading.remove(it)
+                } else {
+                    println("Already downloaded: " + it)
                 }
-                Thread.sleep(5000)
             }
+            Thread.sleep(5000)
         }
 
     })
@@ -70,11 +80,38 @@ fun main(args: Array<String>) {
     readLine()
     executor.shutdownNow()
 
-
-
-
+    val writer = PrintWriter("rut-downloader.json", "UTF-8")
+    writer.write(gson.toJson(downloaded))
+    writer.close()
 
 }
+
+fun cliDownloadTorrent(t: Torrent, c: FTPCredentials) {
+    val tempScript = createDownloadScript(t, c)
+    try {
+        val pb = ProcessBuilder("bash", tempScript.toString())
+        pb.inheritIO()
+        val process = pb.start()
+        process.waitFor()
+    } finally {
+        tempScript.delete()
+    }
+}
+
+fun createDownloadScript(t: Torrent, c: FTPCredentials): File {
+    val tempScript = File.createTempFile("script", null)
+    val streamWriter = OutputStreamWriter(FileOutputStream(tempScript))
+    val printWriter = PrintWriter(streamWriter)
+    printWriter.println("cd " + downloadDirectory)
+    val downloadCommmand = String.format("lftp -c \"mirror --use-pget-n=16 ftp://%s:%s@%s/%s\"", c.userName, c.password, c.url, t.location.replace(c.pathTruncate, ""))
+    println("Downloading: " + downloadCommmand)
+    printWriter.println(downloadCommmand)
+    printWriter.println("exit")
+    printWriter.println("exit")
+    printWriter.close()
+    return tempScript
+}
+
 
 fun getFilesRecursive(initialDir: String, ftp: FTPClient): HashMap<String, Boolean> {
     val directories = HashMap<String, Boolean>()
